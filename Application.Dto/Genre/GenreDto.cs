e0436c514e15b79af7e4e32331a5e9c1f3f15f2e
josephorendo <jgorendo@sigcomt.com>
2022-11-26 11:30:44 -0500

    change

--- Application.Dto/Genre/GenreDto.cs
+++ Application.Dto/Genre/GenreDto.cs
@@ -4,8 +4,6 @@ namespace Application.Dto.Postulant;
 
 public class GenreDto
 {
-    public int Id { get; set; }
-    public string GenreName { get; set; }
     public ICollection<MovieWithoutGenre> Movies { get; set; }
 
 }
